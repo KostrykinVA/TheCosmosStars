fcb3c562e2d10fa9c12eabf72c87e2eb1b26a50c
GVanDerFlex <21missnglink21@gmail.com>
2018-12-14 01:01:20 +0300

    map and its movement

--- Game/main.cpp
+++ Game/main.cpp
@@ -27,14 +27,29 @@ int main(){
 	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
 	sf::RenderWindow window(sf::VideoMode(960, 540, desktop.bitsPerPixel), "Cosmo");
 
+	Image map_image;//объект изображени¤ дл¤ карты
+	map_image.loadFromFile("images/cosmos.png");//загружаем файл дл¤ карты
+	Texture map;//текстура карты
+	map.loadFromImage(map_image);//зар¤жаем текстуру картинкой
+	Sprite mapSprite;//создаЄм спрайт дл¤ карты
+	mapSprite.setTexture(map);//заливаем текстуру спрайтом
+	
+	Clock clock; 
+	
+	float i = 0;
+	
 	while (window.isOpen()){
 		sf::Event event; //Переменная для события
 		while (window.pollEvent(event)) //Опрос событий
 			if (event.type == sf::Event::Closed)
 				window.close();
+		
+		float time = clock.getElapsedTime().asSeconds();
+		if (int(i += time*1000) >= 960) 
+			i = 0;
 
-
-		player.move();
+		mapSprite.setPosition(-i, 0);
+		clock.restart();
 		window.clear(); //Очищаем экран
 		window.display(); 
 	}
