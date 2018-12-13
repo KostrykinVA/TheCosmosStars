bfa1f42cb59cc51676ad0e65f57df989786f54a3
Vladimir <grom.vas@mail.ru>
2018-12-14 00:59:23 +0300

    LLL

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,10 +1,32 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
+
+using namespace sf;
+
+class Entity{
+protected:	
+	Texture texture;
+	Sprite sprite;
+public:
+	Entity(std::string path){}
+
+	Sprite getSprite() {}
+	void setPosition(float x, float y) {}
+};
+
+class Player:public Entity{
+	int hp;
+public:
+	Player(std::string path):Entity(path){}
+	void move(){}
+	void attack(){}
+};
 
 int main(){
 	sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
 	sf::RenderWindow window(sf::VideoMode(960, 540, desktop.bitsPerPixel), "Cosmo");
 
-	// Основной (бесконечный) цикл
 	while (window.isOpen()){
 		sf::Event event; //Переменная для события
 		while (window.pollEvent(event)) //Опрос событий
